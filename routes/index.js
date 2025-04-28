34e80dec630ef2acc8ef876207346e157a288cc4
Bartue <[bartbatiancila28@gmail.com]>
2025-04-29 00:27:09 +0800

    Import signinRoute to index route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const registerRoute = require("./auth/register");
+const signinRoute = require("./auth/signin")
 const postRoute = require("./api/post");
 
 module.exports = {
   registerRoute,
-  postRoute
+  postRoute,
+  signinRoute
 }
