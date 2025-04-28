8eba228320fbe92702aed2bff1427c987ca6ad1f
Bartue <[bartbatiancila28@gmail.com]>
2025-04-29 00:25:20 +0800

    Add getPost, updatePost, deletePost controller to /:postId/user/:userId route

--- routes/api/post.js
+++ routes/api/post.js
@@ -2,9 +2,16 @@ const { Router } = require("express");
 const router = Router();
 const postCon = require("../../controllers/api/postController");
 
+//mock query. userId should not in params. 
+router.route("/:userId")
+  .get(postCon.getAllPost)
+  .post(postCon.createPost)
 
-router.get("/:userId", postCon.getAllPost);
-router.post("/:userId", postCon.createPost);
+
+router.route("/:postId/user/:userId")
+  .get(postCon.getPost)
+  .put(postCon.updatePost)
+  .delete(postCon.deletePost)
 
 module.exports = router;
 
