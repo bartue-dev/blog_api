30e0a30dc1739cd07a8e676c99cec7bc2e9d1233
Bartue <[bartbatiancila28@gmail.com]>
2025-04-30 22:39:10 +0800

    Add comments to each request route

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,16 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 const postCon = require("../../controllers/api/postController");
+ 
+//api route
 
-//mock query. userId should not in params. 
-router.route("/:userId")
+router.route("/")
+  //get all post
   .get(postCon.getAllPost)
+  //create post
   .post(postCon.createPost)
 
-
-router.route("/:postId/user/:userId")
+router.route("/:postId/")
+  //get specific post with postId params
   .get(postCon.getPost)
+  //update specific post with postId params
   .put(postCon.updatePost)
+  //delete specific post with postId params
   .delete(postCon.deletePost)
 
 module.exports = router;
