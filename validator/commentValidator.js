0a2c868e02bb059413836f73e312ec2c305bdff5
Bartue <[bartbatiancila28@gmail.com]>
2025-05-03 10:30:04 +0800

    Create an comment validation

---
+++ validator/commentValidator.js
@@ -0,0 +1,47 @@
+const { body, param } = require("express-validator");
+
+const isEmptyMsg = "must not be empty";
+
+const validateCreateComment = [
+  body("content").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`),
+  param("postId")
+    .exists().withMessage("postId doesn't exist")
+    .isUUID().withMessage("postId must be a valid UUID") 
+]
+
+const validateCreateChildComment = [
+  body("content").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`),
+  param("commentId")
+    .exists().withMessage("commentId doesn't exist")
+    .isUUID().withMessage("commentId must be a valid UUID")
+]
+
+const validateGetAllComments = [
+  param("postId")
+    .exists().withMessage("postId doesn't exist")
+    .isUUID().withMessage("postId must be a valid UUID")
+]
+
+const validateUpdateComment = [
+  body("content").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`),
+  param("commentId")
+  .exists().withMessage("commentId doesn't exist")
+  .isUUID().withMessage("commentId must be a valid UUID")
+]
+
+const validateDeleteComment = [
+  param("commentId")
+  .exists().withMessage("commentId doesn't exist")
+  .isUUID().withMessage("commentId must be a valid UUID")
+]
+
+module.exports = {
+  validateCreateComment,
+  validateCreateChildComment,
+  validateGetAllComments,
+  validateUpdateComment,
+  validateDeleteComment
+}
