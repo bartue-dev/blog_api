a1863a0c8b84d940c76c9d5a2deee496d1215d17
Bartue <[bartbatiancila28@gmail.com]>
2025-05-03 10:24:24 +0800

    Added an authQuries. Handles all the queries for account, user, refreshToken model from db

---
+++ db/authQueries.js
@@ -0,0 +1,96 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+//account queries
+class Account {
+  async createAccount(username, password, email) {
+      return await prisma.account.create({
+        data: {
+          username: username,
+          password: password,
+          email: email,
+          user: {
+            create: {
+              //creates a accountId in the user model automatically
+              username: username
+            } 
+          }
+        },
+        include: {
+          user: true
+        }
+     });  
+  }
+
+  async currentAccountByUsername(username) {
+    return await prisma.account.findUnique({
+      where: {
+        username: username
+      }
+    });
+  }
+}
+
+//user queries
+class User {
+  async getUser(userId) {
+    return await prisma.user.findUnique({
+      where: {
+        accountId: userId
+      },
+      //used omit clause to exclude the userId from the result
+      //also select clause is opposite to omit
+      omit: {
+        userId: true
+      }
+    });
+  }
+}
+
+//refresh token queries
+class refreshToken {
+  //note: refresh token should be deleted/update if it exprires
+  async saveToken(accountId, refreshToken) {
+    return await prisma.refreshToken.upsert({
+      where: {
+        accountId: accountId
+      },
+      update: {
+        refreshToken: refreshToken
+      },
+      create: {
+        refreshToken: refreshToken,
+        accountId: accountId
+      }
+    });
+  }
+
+  //current user by refresh token
+  async currentAccountByToken(refreshToken) {
+    return await prisma.refreshToken.findFirst({
+      where: {
+        refreshToken: refreshToken
+      }
+    });
+  }
+
+  //delete refresh token
+  async deleteRefreshToken(refreshToken) {
+    return await prisma.refreshToken.delete({
+      where: {
+        refreshToken: refreshToken
+      }
+    });
+  }
+}
+
+const accountMethods = new Account();
+const userMethods = new User();
+const refreshTokenMethods = new refreshToken();
+
+module.exports = {
+  accountMethods,
+  refreshTokenMethods,
+  userMethods
+}
