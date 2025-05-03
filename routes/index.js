666597b2b3dc2eac4efe5686bf9b7d099efebf05
Bartue <[bartbatiancila28@gmail.com]>
2025-05-03 10:22:45 +0800

    Import comment route to index.js route

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,15 @@ const signinRoute = require("./auth/signin")
 const refreshTokenRoute = require("./auth/refreshToken");
 const logoutRoute = require("./auth/logout");
 
-
 //api
 const postRoute = require("./api/post");
+const commentRoute = require("./api/comment");
 
 module.exports = {
   registerRoute,
-  postRoute,
   signinRoute,
   refreshTokenRoute,
-  logoutRoute
+  logoutRoute,
+  postRoute,
+  commentRoute
 }
