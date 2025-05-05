201af5e84b1e923ea11c4ed9d04d69a627287b30
Bartue <[bartbatiancila28@gmail.com]>
2025-05-05 21:28:25 +0800

    Creates a validation for likedPost controller

---
+++ validator/likedPostValidator.js
@@ -0,0 +1,22 @@
+const { param } = require("express-validator");
+
+const validateSaveLikedPost = [
+  param("postId")
+    .exists().withMessage("postId doesn't exist")
+    .isUUID().withMessage("postId is not a valid UUID"),
+  param("likedId")
+  .exists().withMessage("likedId doesn't exist")
+  .isUUID().withMessage("likedId is not a valid UUID"),
+]
+
+const validateUndoLikedPost = [
+  param("likedId")
+  .exists().withMessage("likedId doesn't exist")
+  .isUUID().withMessage("likedId is not a valid UUID"),
+]
+
+
+module.exports = {
+  validateSaveLikedPost,
+  validateUndoLikedPost
+}
